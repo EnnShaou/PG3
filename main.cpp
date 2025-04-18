e1ef56f592a30e8b0ea9deebbfdd411d50b87ab2
EnnShaou <k024g1013@g.neec.ac.jp>
2025-04-18 15:43:06 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 //一般的な貸金計算関数
 //この関数は、時間が0以下の場合は0を返し、それ以外の場合は、給与と時間を引数にして、給与を加算していく再帰的な計算を行います。
 //引数のsalaryは給与、hoursは時間を表します。
-int GeneralSalary(int salary,int hours) {
-	if (hours<=0)
+int GeneralSalary(int salary, int hours) {
+	if (hours <= 0)
 	{
 		return 0;
 	}
-	return (salary+GeneralSalary(salary, hours-1));
+	printf("%d時間働くと給料は%d円\n", hours, salary);
+	return (salary+ GeneralSalary(salary, hours - 1));
 }
 //再帰的な貸金計算関数
 //この関数は、時間が0以下の場合は0を返し、それ以外の場合は、給与と時間を引数にして、給与を2倍にして50を引いた値を再帰的に計算します。
@@ -19,24 +20,27 @@ int ReturnSalary(int salary, int hours) {
 	{
 		return 0;
 	}
-	return (salary + ReturnSalary(salary*2-50, hours - 1));
+	printf("%d時間働くと給料は%d円\n",hours, salary);
+	return (salary + ReturnSalary(salary * 2 - 50, hours - 1));
 }
 
-int main(){
-	
-	int hours = 0;
+int main() {
+
+	int hours = 8;
 	//一般的な貸金計算関数と再帰的な貸金計算関数を比較するためのループ
-	while(1) {
+	/*while (1) {
 		hours++;
 		printf("%d\n", GeneralSalary(1024, hours));
 		printf("%d\n", ReturnSalary(100, hours));
-		if (GeneralSalary(1024, hours)< ReturnSalary(100, hours))
+		if (GeneralSalary(1024, hours) < ReturnSalary(100, hours))
 		{
 			printf("%d", hours);
 			break;
 		}
 
-	}
-	
+	}*/
+
+	printf("一般的貸金体系%d\n", ReturnSalary(100, hours));
+	printf("再帰的貸金体系%d\n", GeneralSalary(1024, hours));
 	return 0;
 }
