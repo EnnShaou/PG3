1f16f880b1926b774aed48bfad6bab1f9dd4dd69
EnnShaou <k024g1013@g.neec.ac.jp>
2025-06-05 14:05:28 +0900

    抽象クラスとインターフェイス

--- main.cpp
+++ main.cpp
@@ -1,7 +1,66 @@
 #include<stdio.h>
 #include<Windows.h>
+class IsShape
+{
+public:
+	IsShape() :area(0.0f) {}
+	virtual ~IsShape() {};
+	virtual void Draw() = 0;
+	virtual void Size() = 0;
 
-int main(){
-	
+protected:
+	float area;
+};
+class Circle : public IsShape
+{
+public:
+	Circle(float radius) :radius_(radius) {}
+	~Circle() {}
+	void Draw() {
+		printf("Circle area=%f\n", area);
+	}
+	void Size() {
+		area = 3.14f * radius_ * radius_;
+	}
+private:
+	float radius_;
+};
+
+class Rect : public IsShape
+{
+public:
+	Rect(float length, float width) :
+		length_(length), width_(width)
+	{
+	}
+	~Rect() {
+	}
+	void Draw() {
+		printf("Rectangle area=%f\n", area);
+	}
+	void Size() {
+		area = length_ * width_;
+
+	}
+private:
+	float length_;
+	float width_;
+};
+
+
+int main() {
+
+	IsShape* shape = new Circle(5.0f);
+	shape->Size();
+	shape->Draw();
+	delete shape;
+
+	shape = new Rect(5.0f, 10.0f);
+	shape->Size();
+	shape->Draw();
+	delete shape;
+
 	return 0;
 }
+
+
