41eb632690bcdbba95649038a349b9cb5866f662
EnnShaou <k024g1013@g.neec.ac.jp>
2025-06-06 16:50:11 +0900

    コメント

--- main.cpp
+++ main.cpp
@@ -3,23 +3,30 @@
 class IsShape
 {
 public:
+	// 抽象クラス
 	IsShape() :area(0.0f) {}
 	virtual ~IsShape() {};
+	// 純粋仮想関数
 	virtual void Draw() = 0;
+	// 面積を計算する純粋仮想関数
 	virtual void Size() = 0;
 
 protected:
+	// 面積を保持する変数
 	float area;
 };
+// Shapeクラスを継承したCircleとRectクラスの実装
 class Circle : public IsShape
 {
 public:
+
 	Circle(float radius) :radius_(radius) {}
 	~Circle() {}
 	void Draw() {
 		printf("Circle area=%f\n", area);
 	}
 	void Size() {
+		// 面積を計算する
 		area = 3.14f * radius_ * radius_;
 	}
 private:
@@ -36,29 +43,29 @@ public:
 	~Rect() {
 	}
 	void Draw() {
-		printf("Rectangle area=%f\n", area);
+		printf("Rectangle area=%f\n", area);// 面積を表示
 	}
 	void Size() {
+		// 面積を計算する
 		area = length_ * width_;
 
 	}
 private:
-	float length_;
-	float width_;
+	float length_;// 長さ
+	float width_;// 幅
 };
 
 
 int main() {
-
-	IsShape* shape = new Circle(5.0f);
-	shape->Size();
-	shape->Draw();
-	delete shape;
-
-	shape = new Rect(5.0f, 10.0f);
-	shape->Size();
-	shape->Draw();
-	delete shape;
+	IsShape* shape = new Circle(5.0f);// Circleのインスタンスを作成
+	shape->Size();// 面積を計算
+	shape->Draw();// 面積を表示
+	delete shape;// メモリを解放
+	// 次のインスタンスを作成
+	shape = new Rect(5.0f, 10.0f);// 長方形のインスタンスを作成
+	shape->Size();// 面積を計算
+	shape->Draw();//面積を表示
+	delete shape;// メモリを解放
 
 	return 0;
 }
