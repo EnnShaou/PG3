ca9513c876edfc65f82bfeeb9365efaeaeba1fe8
EnnShaou <k024g1013@g.neec.ac.jp>
2025-08-01 14:55:37 +0900

    std::thread

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include<stdio.h>
 #include<Windows.h>
+#include<thread>
 
-int main(){
-	
+void thread_1() {
+	printf("Thread1\n");
+}
+void thread_2() {
+	printf("Thread2\n");
+
+}
+void thread_3() {
+	printf("Thread3\n");
+}
+
+int main() {
+	// Create three threads and run them concurrently
+	std::thread t1(thread_1);
+	t1.join();
+	std::thread t2(thread_2);
+	t2.join();
+	std::thread t3(thread_3);
+	t3.join();
 	return 0;
 }
